02f95235b240e501695dfea667378be1117defb2
Neil Vallon <neilvallon@gmail.com>
2018-05-07 00:13:08 -0500

    add formula

---
+++ fakecoin.rb
@@ -0,0 +1,59 @@
+class Fakecoin < Formula
+  homepage "https://fakco.in/"
+  url "https://github.com/Fake-Coin/FakeCoin-Qt.git"
+
+  #head do
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  #end
+
+  depends_on "pkg-config" => :build
+  depends_on "berkeley-db@4"
+  depends_on "boost"
+  depends_on "libevent"
+  depends_on "miniupnpc"
+  depends_on "openssl"
+  depends_on "zeromq"
+
+  needs :cxx11
+
+  def install
+    if MacOS.version == :el_capitan && MacOS::Xcode.installed? &&
+       MacOS::Xcode.version >= "8.0"
+       ENV.delete("SDKROOT")
+    end
+
+    system "./autogen.sh" # if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-boost-libdir=#{Formula["boost"].opt_lib}",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    pkgshare.install "share/rpcuser"
+  end
+
+  plist_options :manual => "fakecoind"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/fakecoind</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/test_fakecoin"
+  end
+end
